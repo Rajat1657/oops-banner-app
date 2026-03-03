5b18271f7ac839571b86ccb692953dd201fbc259
Rajat1657 <rajat16m5@gmail.com>
2026-03-03 13:39:17 +0530

    UC2 Updated

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,22 @@
+
+/**
+ * OOPS Banner App
+ * UC2 - Print OOPS in Banner Format
+ * @author Rajat
+ * @version 2.0
+ */
+
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+           System.out.println("   " + "***" + "     " + "***" + "     ******     *****  ");
+           System.out.println("  **  **   " + "**" + "  **   **   **   **   ");
+           System.out.println(" **    ** " + "**" + "    **  **   **   **      ");
+           System.out.println(" **    ** " + "**" + "    **  ******      " + "***" + "   ");
+           System.out.println(" **    ** " + "**" + "    **  **             **");
+           System.out.println("  **  **   " + "**" + "  **   **             **");
+           System.out.println("   " + "***" + "      ***      **       *****   ");
+
     }
 }
+
